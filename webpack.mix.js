9491161a5edea23f9548485a9a19de4b405bd2a3
Gabriel Vilches Alves <vilchesalves@gmail.com>
2019-03-18 21:33:42 +1300

    setup eslint

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const mix = require('laravel-mix');
 
 /*
@@ -12,4 +13,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css');
