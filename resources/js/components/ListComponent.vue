fd37126368e190efdab276dc0eda187bf78d0279
Gabriel Vilches Alves <vilchesalves@gmail.com>
2019-03-21 22:50:16 +1300

    paginate dashboard

--- resources/js/components/ListComponent.vue
+++ resources/js/components/ListComponent.vue
@@ -1,17 +1,33 @@
 <template>
-  <ul>
-    <li
-      v-for="post in posts"
-      :key="post.id"
-    >
-      <a
-        :href="`/dashboard/edit/${post._id}`"
-        @click.prevent="edit(post._id)"
+  <div>
+    <ul>
+      <li
+        v-for="post in posts"
+        :key="post.id"
       >
-        {{ post.title }}
-      </a>
-    </li>
-  </ul>
+        <a
+          :href="`/dashboard/edit/${post._id}`"
+          @click.prevent="edit(post._id)"
+        >
+          {{ post.title }}
+        </a>
+      </li>
+    </ul>
+    <button
+      v-if="prevPageUrl"
+      class="btn btn-primary"
+      @click="load(prevPageUrl)"
+    >
+      Previous Page
+    </button>
+    <button
+      v-if="nextPageUrl"
+      class="btn btn-primary"
+      @click="load(nextPageUrl)"
+    >
+      Next Page
+    </button>
+  </div>
 </template>
 
 <script>
@@ -21,19 +37,41 @@ export default {
   data() {
     return {
       posts: [],
+      path: '/api/v1/posts',
+      prevPageUrl: '',
+      nextPageUrl: '',
     };
   },
   async mounted() {
     const {
       data: {
         data: posts,
+        next_page_url: nextPageUrl,
+        prev_page_url: prevPageUrl,
       },
-    } = await axios('/api/v1/posts');
+    } = await axios(this.path);
+
     this.posts = posts;
+    this.nextPageUrl = nextPageUrl;
+    this.prevPageUrl = prevPageUrl;
   },
   methods: {
     edit(id) {
       this.$emit('edit', id);
+    },
+    async load(url) {
+      const {
+        data: {
+          data: posts,
+          next_page_url: nextPageUrl,
+          prev_page_url: prevPageUrl,
+        },
+      } = await axios(url);
+
+      this.path = url;
+      this.posts = posts;
+      this.nextPageUrl = nextPageUrl;
+      this.prevPageUrl = prevPageUrl;
     },
   },
 };
